de075b9f497d5a32314c2518b6d8b43db69d6c4c
201411108 <aggsae@gmail.com>
2021-07-26 22:28:30 +0900

    feat : ADD router for pages
    로그인 및 홈 화면 구성을 위한 리액트 라우터 구현

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,12 +1,17 @@
-import React from 'react';
-import './App.css';
+import React from 'react'
+import { Route } from 'react-router-dom'
+import HomePage from 'Pages/HomePage'
+import LoginPage from 'Pages/LoginPage'
+import './App.css'
 
 function App() {
   return (
     <div className="App">
       손절이 나의 멘탈을 지킨다.
+      <Route exact path='/' component={LoginPage} />
+      <Route path='/home' component={HomePage} />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
