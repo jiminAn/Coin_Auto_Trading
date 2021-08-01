a236279de175e63e9b85002684e75bf6cf9a8595
pmsu2007 <75938496+pmsu2007@users.noreply.github.com>
2021-08-01 20:51:14 +0900

    Delete back-end directory

--- back-end/config.py
+++
@@ -1,6 +0,0 @@
-import os
-
-BASE_DIR = os.path.dirname(__file__)
-
-SQLALCHEMY_DATABASE_URI = 'sqlite:///{}'.format(os.path.join(BASE_DIR, 'AutoCoinTrading.db'))
-SQLALCHEMY_TRACK_MODIFICATIONS = False
--- back-end/trading/views/main_views.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint
-
-bp = Blueprint('main', __name__, url_prefix='/')
-
-
-@bp.route('/') # app.route가 아닌 블루프린트 클래스 객체 bg.route로 표시한다.
-
-def hello_pybo():
-    return 'Hello, Pybo!'
