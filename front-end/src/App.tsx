255bb0a81fb8ad96743a18e80bb99d6038a04b67
201411108 <aggsae@gmail.com>
2021-08-03 22:19:22 +0900

    feat: Add GlobalNavigationBar components
    상단 네비게이션 바 컴포넌트 생성
    feature 브렌치 종료료

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -2,12 +2,13 @@ import React from 'react'
 import { Route } from 'react-router-dom'
 import HomePage from 'Pages/HomePage'
 import LoginPage from 'Pages/LoginPage'
+import GlobalNavigationBar from 'Components/GlobalNavigationBar'
 import './App.css'
 
 function App() {
   return (
     <div className="App">
-      손절이 나의 멘탈을 지킨다.
+      <GlobalNavigationBar />
       <Route exact path='/' component={LoginPage} />
       <Route path='/home' component={HomePage} />
     </div>
