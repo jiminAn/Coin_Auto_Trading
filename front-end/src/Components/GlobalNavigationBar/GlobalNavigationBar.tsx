da2004ad9391ebd7fd833336dbd15f89a039fefa
201411108 <aggsae@gmail.com>
2021-08-06 15:46:11 +0900

    feat: Global Navigation Bar 화면 구성 및 로그인 여부에 따른 로직 구현
    Global Navigation Bar 화면 요소 구성
    로고를 눌렀을 때 로그인 여부에 따라서 서로 다른 화면으로 이동하는 로직
    구현현

--- front-end/src/Components/GlobalNavigationBar/GlobalNavigationBar.tsx
+++ front-end/src/Components/GlobalNavigationBar/GlobalNavigationBar.tsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import './GlobalNavigationBar.css'
 
-function GlobalNavigationBar() {
+interface LoginInfo {
+    isLogin: boolean
+}
+
+function GlobalNavigationBar({ isLogin }: LoginInfo) {
     return (
-        <div>손절이 나의 멘탈을 지킨다.</div>
+        <div className="NavigationContainer">
+            <nav>
+                <div className='title'>
+                    { !isLogin ?
+                        <a href='/'>손절이 나의 멘탈을 지킨다.</a> :
+                        <a href='/home'>손절이 나의 멘탈을 지킨다.</a>
+                    }
+                </div>
+                <ul>
+                    <li><div className='menu'>자동거래</div></li>
+                    <li><div className='menu'>거래소</div></li>
+                    <li><div className='menu'>도움말</div></li>
+                </ul>
+            </nav>
+        </div>
     )
 }
 
