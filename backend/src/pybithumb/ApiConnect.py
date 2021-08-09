261e0ed89d9346f977030056b1bb0bd524e75bdf
comeeasy <zz1236zz@naver.com>
2021-08-10 01:25:40 +0900

    Update ClientAsset, add method connecting websockets, some logging statments

--- backend/src/pybithumb/ApiConnect.py
+++ backend/src/pybithumb/ApiConnect.py
@@ -1,4 +1,5 @@
 import pybithumb
+import logging
 
 class Connect :
     """
@@ -11,20 +12,23 @@ class Connect :
         """
         initialize connect key & secret key
         """
+
         with open("./key.txt", "r") as f:
-            self._con_key = f.readline().strip()
-            self._sec_key = f.readline().strip()
+            self.__con_key = f.readline().strip()
+            self.__sec_key = f.readline().strip()
+
+        logging.info(f"Login private API with connect key: {self.__con_key}")
 
     def get_bitumb(self):
         """
         initialize client Bithumb instance
         :return: Bithumb instance
         """
-        return pybithumb.Bithumb(self._con_key, self._sec_key)
+        return pybithumb.Bithumb(self.__con_key, self.__sec_key)
 
     def get_con_key(self):
         """
         get connect key for API
         :return: con_key (String)
         """
-        return self._con_key
+        return self.__con_key
--- backend/src/pybithumb/ClientAsset.py
+++ backend/src/pybithumb/ClientAsset.py
@@ -1,25 +1,87 @@
 import pybithumb
 from collections import defaultdict
 from ApiConnect import Connect
+from threading import Thread
 
+import logging
+import time
+import sys
+from tqdm import tqdm
 
-class ClientAsset:
+logging.basicConfig(stream=sys.stdout, level=logging.INFO,
+                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+class ClientAsset(Thread):
     """
     load the Client Asset information
     """
-    _ticker_dict = defaultdict(list)
-    _client_api = None
 
-    def __init__(self):
+    def __init__(self, name):
         """
         inquire all tickers and save Client's asset information
         """
+        super().__init__()
+        self.name = name
+        logging.info(f"Name: {self.name}")
+
         self._client_api = Connect()
-        tickers = pybithumb.get_tickers()
-        for ticker in tickers:
-            balance = self._client_api.get_bitumb().get_balance(ticker)
-            if balance[0] > 0.0:
-                self._ticker_dict[ticker].append(balance)
+        self._ticker_dict = defaultdict(list)
+
+        logging.info("Checking valid tickers (possesed tickers)...")
+        self._tickers = ['ETH']
+
+        # for ticker in tqdm(pybithumb.get_tickers()):
+        #     balance = self._client_api.get_bitumb().get_balance(ticker)
+        #     if balance[0] > 0.0:
+        #         self._ticker_dict[ticker].append(balance)
+        #         self._tickers.append(ticker)
+
+        # connecting websockets
+        self._ws_ticker = None
+        self._ws_transaction = None
+        self._ws_orderbookdepth = None
+        self._connect_websockets()
+
+        # start update data
+        self.start()
+        # self._ws_thread = Thread(target=self._update_data)
+        # self._ws_thread.start()
+
+    def _connect_websockets(self):
+        logging.info("Connecting websockets for possesed coins...")
+
+        # pybithumb sopports only KRW market
+        ws_tickers = [ticker + '_KRW' for ticker in self._tickers]
+        logging.info(f"Subscribe {ws_tickers}")
+        self._ws_ticker = pybithumb.WebSocketManager("ticker", ws_tickers)
+        self._ws_transaction = pybithumb.WebSocketManager("transaction", ws_tickers)
+        self._ws_orderbookdepth = pybithumb.WebSocketManager("orderbookdepth", ws_tickers)
+
+        if self._ws_ticker is None or self._ws_transaction is None or self._ws_orderbookdepth is None:
+            logging.error(f"_ws_ticker          connection: {not self._ws_ticker is None}")
+            logging.error(f"_ws_transaction     connection: {not self._ws_transaction is None}")
+            logging.error(f"_ws_orderboookdepth connection: {not self._ws_orderbookdepth is None}")
+            raise ConnectionError("Wecsockets are not connected..")
+        else:
+            logging.info("Websockets are connected successfully")
+
+
+    def run(self) -> None:
+        while True:
+            num = input("enter to get data")
+
+            data_ticker = self._ws_ticker.get()
+            data_transaction = self._ws_transaction.get()
+            data_orderbookdepth = self._ws_orderbookdepth.get()
+
+            print("=== data ticker ====")
+            print(data_ticker); print('\n')
+
+            print("=== data transaction ====")
+            print(data_transaction); print('\n')
+
+            print("=== data orderbookdepth ====")
+            print(data_orderbookdepth); print('\n')
 
     def get_ticker(self):
         """
@@ -36,7 +98,10 @@ class ClientAsset:
         return self._ticker_dict
 
 if __name__ == "__main__":
-    client = ClientAsset()
+    client = ClientAsset("joono")
+
+    num = input("enter number")
+    print(num)
 
     print(client.get_ticker())
     print(client.get_ticker_dict())
