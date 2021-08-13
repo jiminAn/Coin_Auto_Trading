befa2e117d6d5f0309fc3dbf55a384200e30d007
201411108 <aggsae@gmail.com>
2021-08-13 22:58:46 +0900

    feat: add form and FindingAPIKey
    api key 입력 폼 및 버튼, api key 찾기 컴포넌트 추가

--- front-end/src/Pages/LoginPage.tsx
+++ front-end/src/Pages/LoginPage.tsx
@@ -1,11 +1,18 @@
 import React from 'react'
-import LoginInfo from '../Components/LoginPage/LoginInfo'
+import LoginInfo from 'Components/LoginPage/LoginInfo'
+import FindingAPIKey from 'Components/LoginPage/FindingAPIKey'
 import './LoginPage.css'
 
 function LoginPage() {
     return (
         <div className='loginContainer'>
             <LoginInfo />
+            <form>
+                <input type='password' placeholder='public API key를 입력해주세요.' /><br/>
+                <input type='password' placeholder='private API key를 입력해주세요.' />
+            </form>
+            <button type='button' onClick={ () => console.log('click') }>Login</button>
+            <FindingAPIKey />
         </div>
     )
 }
