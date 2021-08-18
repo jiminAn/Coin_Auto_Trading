4ded522441fc70be34eb1c9f626c1499f5531be1
jiminAn <als398@naver.com>
2021-08-18 17:41:17 +0900

    remove:  delete src directory in root path

--- src/views/main_views.py
+++
@@ -1,9 +0,0 @@
-from flask import Blueprint, url_for
-from werkzeug.utils import redirect
-
-bp = Blueprint('main', __name__, url_prefix='/')
-
-
-@bp.route('/')
-def index():
-    return redirect(url_for('coin.start'))
