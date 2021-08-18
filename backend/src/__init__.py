570d9c9f661ad0c9e12732a5a2af2c6110fba5e2
안지민 <>
2021-08-18 17:23:23 +0900

    refactor: add db app, db table name

--- backend/src/__init__.py
+++ backend/src/__init__.py
@@ -1,13 +1,15 @@
 from flask import Flask
 from src.database import db, migrate
 from . import config
-
-from src.model.models import client_information
-from src.model.models import coin_information
+from src.model.models import Client
+from src.model.models import Coin
 
 def create_app(env):
     app = Flask(__name__)
     app.config.from_object(config)
     db.init_app(app)
+    with app.app_context():
+        db.create_all()
+    app.app_context().push()
     migrate.init_app(app, db)
     return app
