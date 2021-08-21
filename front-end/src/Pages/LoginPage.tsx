0bd84e1ceaa57339fea107acc8b375a4671c8ce0
201411108 <aggsae@gmail.com>
2021-08-21 13:44:21 +0900

    style: add className in api input

--- front-end/src/Pages/LoginPage.tsx
+++ front-end/src/Pages/LoginPage.tsx
@@ -4,12 +4,21 @@ import FindingAPIKey from 'Components/LoginPage/FindingAPIKey'
 import './LoginPage.css'
 
 function LoginPage() {
+    // TODO :: input 태그의 값을 담기 위한 hook 설정
+    // const useInput = () => {
+    //     return
+    // }
+
+    // const clickEventHandler = () => {
+
+    // }
+
     return (
         <div className='loginContainer'>
             <LoginInfo />
             <form>
-                <input type='password' placeholder='public API key를 입력해주세요.' /><br/>
-                <input type='password' placeholder='private API key를 입력해주세요.' />
+                <input type='password' placeholder='public API key를 입력해주세요.' className='apiInput'/><br/>
+                <input type='password' placeholder='private API key를 입력해주세요.' className='apiInput'/>
             </form>
             <button type='button' onClick={ () => alert('click') }>Login</button>
             <FindingAPIKey />
