47decc6bf6ffafe45d667eacf90da4e28a2b38c3
jiminAn <als398@naver.com>
2021-08-25 13:54:33 +0900

    refactor: remove create_asset_name_dict() method

--- backend/src/crawling/CrawledInfo.py
+++ backend/src/crawling/CrawledInfo.py
@@ -9,39 +9,20 @@ def create_ticker_name_dict():
     :return: tickers['ticker'] = name
     """
     cnt = 0
-    limit = 20
+    limit = 20 # the limited number of tickers
     tickers = defaultdict()
     with open('../tickernames.txt', 'r', encoding= 'utf-8') as f:
         for line in f.readlines():
             cnt += 1
             ticker = re.findall('[a-zA-Z].*',line)[0]
             ko_name = line.replace(ticker,'').strip()
-            #print(ticker, ':', ko_name) # for CHECKING
-
+            
             tickers[ticker] = ko_name
             if cnt == limit:
                 break
 
     return tickers
 
-
-def create_asset_name_dict(asset_list):
-    list = asset_list
-    tickers = defaultdict()
-    index = 0
-    limit = len(list)
-    with open('../tickernames.txt', 'r', encoding='utf-8') as f:
-        for line in f.readlines():
-            ticker = re.findall('[a-zA-Z].*', line)[0]
-            ko_name = line.replace(ticker, '').strip()
-            if list[index] == ticker:
-                index += 1
-                tickers[ticker] = ko_name
-
-            if index == limit:
-                break
-    return tickers
-
 if __name__ == "__main__":
-     tickers_ko_naming = create_asset_name_dict()
+     tickers_ko_naming = create_ticker_name_dict()
      pp.pprint(tickers_ko_naming)
