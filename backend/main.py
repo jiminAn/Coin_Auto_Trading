7acb65f58b8a179700c3d5891bebf1fc6c5eb64b
Daehoon <foreat_14@naver.com>
2021-08-31 16:49:13 +0900

    fix: 윈도우 환경에서 멀티프로세싱 가능하게 수정

--- backend/main.py
+++ backend/main.py
@@ -2,7 +2,6 @@
 from src import create_app
 from src.pybithumb.RealTimeWebsocketProcess import RealTimeWebsocketProcess
 from pybithumb import Bithumb
-#from src.model.models import User
 app = create_app("dev")
 
 if __name__ == '__main__':
--- backend/src/pybithumb/RealTimeWebsocketProcess.py
+++ backend/src/pybithumb/RealTimeWebsocketProcess.py
@@ -2,7 +2,7 @@ from collections import defaultdict
 
 # from src.ApiConnect import Connect
 from multiprocessing import Process
-from tqdm import tqdm # for progress bar
+from tqdm import tqdm  # for progress bar
 import multiprocessing as mp
 # import pybithumb
 import logging
@@ -12,14 +12,13 @@ import websockets
 import asyncio
 import json
 
-
 logging.basicConfig(stream=sys.stdout, level=logging.INFO,
                     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 
-class RealTimeWebsocketProcess(Process):
+
+class RealTimeWebsocketProcess():
     """
     connecting real time websocket processed
-
     If you want to know information of each type's content please visit below website
     https://apidocs.bithumb.com/docs/websocket_public
     """
@@ -27,60 +26,56 @@ class RealTimeWebsocketProcess(Process):
     def __init__(self, tickers: list):
         """
         ticker: tickers the client possessed
-
         example: websocket_process = RealtimeWebsocketProcess(["BTC", "ETH"])
         """
-        super().__init__()
 
         # tickers possessed by client
-        self._tickers = tickers # for RUN CHECKING
+        self._tickers = tickers  # for RUN CHECKING
 
         # connecting websockets
-        self.types = ["ticker", "transaction", "orderbookdepth"]
+        self._types = ["ticker", "transaction", "orderbookdepth"]
         self._alive = {"ticker": False, "transaction": False, "orderbookdepth": False}
-        self.q = {"ticker": mp.Queue(1000), "transaction": mp.Queue(1000), "orderbookdepth": mp.Queue(1000)}
-        self._connect_websockets()
-
-        self.start()
+        self._q = {"ticker": mp.Queue(1000), "transaction": mp.Queue(1000), "orderbookdepth": mp.Queue(1000)}
+        self.request_types_info()
+        self.get_tikcer_info()
 
     def run(self) -> None:
         """
         0.1초 간격으로 queue에 각 websockets으로 부터 받아온 data가 있으면 이를 가져옵니다.
         websocket의 type의 종류에는 "ticker(현재가)", "transaction(체결)", "orderbookdepth(변경호가)"
         의 data를 받아옵니다.
-
         필요하신 종류의 type이 있다면 self._q[type].get() 과 같이 data를 받아올 수 있습니다.
         Usage:
             if self._q["ticker"] and self._q["transaction"] and self._q["orderbookdepth"]:
-                for type in self._types:
+                for type in self.__types:
                     print(type, '\n', self._q[type].get())
         """
 
         while True:
-            if self.q["ticker"]:
-                data = self.q['ticker'].get()
+            if self._q["ticker"]:
+                data = self._q['ticker'].get()
                 # for tickers in client.tickers:
 
-                if data['content']['symbol'] == 'DOGE_KRW':
+                if data['content']['symbol'] == 'DOGE_KRW':  # DB에 있는 ticker 불러와서 읽어들이게 수정
                     print(data)
 
             time.sleep(0.1)
 
-    def _connect_websockets(self):
+    def request_types_info(self):
         logging.info("Connecting websockets for possesed coins...")
 
         # pybithumb supports only KRW market
         ws_tickers = [ticker + '_KRW' for ticker in self._tickers]
         logging.info(f"Subscribe {ws_tickers}")
 
-        for type in self.types:
-            self._ws_ticker_process = Process(name="ticker", target=self.__start_websocket, args=(type, ws_tickers,))
+        for type in self._types:
+            self._ws_ticker_process = Process(name=type, target=self.get_type_info, args=(type, ws_tickers,))
             self._ws_ticker_process.start()
             logging.info(f'start websocket {type} process')
 
         logging.info("Websockets are connected successfully")
 
-    async def __connect_websocket(self, type, symbols):
+    async def connect_websocket(self, type, symbols):
         """
         original code:
             https://github.com/sharebook-kr/pybithumb/blob/master/pybithumb/websocket.py
@@ -91,12 +86,12 @@ class RealTimeWebsocketProcess(Process):
         async with websockets.connect(uri, ping_interval=None) as websocket:
             connection_msg = await websocket.recv()
             # {"status":"0000","resmsg":"Connected Successfully"}
-            if "Connected Successfully" not in connection_msg :
+            if "Connected Successfully" not in connection_msg:
                 print("connection error")
 
             data = {
-                "type"     : type,
-                'symbols'  : symbols,
+                "type": type,
+                'symbols': symbols,
                 'tickTypes': ["1H"]
             }
             await websocket.send(json.dumps(data))
@@ -108,18 +103,21 @@ class RealTimeWebsocketProcess(Process):
 
             while self._alive[type]:
                 recv_data = await websocket.recv()
-                self.q[type].put(json.loads(recv_data))
+                self._q[type].put(json.loads(recv_data))
 
-    def __start_websocket(self, type, symbols):
+    def start_websocket(self, type, symbols):
         self._alive[type] = True
-        self.__aloop = asyncio.get_event_loop()
-        self.__aloop.run_until_complete(self.__connect_websocket(type, symbols))
+        self._aloop = asyncio.get_event_loop()
+        self._aloop.run_until_complete(self.connect_websocket(type, symbols))
+
+    # MultiProcessing 실행 함수
+    def get_type_info(self, type, ws_tickers):
+        self.start_websocket(type, ws_tickers)
+
+    def get_tikcer_info(self):
+        self._ws_ticker_process = Process(name="ticker", target=self.run)
+        self._ws_ticker_process.start()
 
     def terminate(self):
-        for type in self.types:
+        for type in self._types:
             self._alive[type] = False
-        super().terminate()
-
-
-
-
