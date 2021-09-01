461fe1268454483c9a49d4bd80569d614caf1bd1
unknown <pmsu2007@naver.com>
2021-09-01 13:44:01 +0900

    fix : circular import error

--- backend/src/__init__.py
+++ backend/src/__init__.py
@@ -1,16 +1,17 @@
 from flask import Flask
 from src.database import db, migrate
 from . import config
-from src.model.models import Coin,Client
-# from src.views import coin_views,main_views
+from src.model.models import Coin, Client
+
 
 def create_app(env):
     app = Flask(__name__)
     app.config.from_object(config)
     db.init_app(app)
     migrate.init_app(app, db)
-    
-    # ERROR
-    # app.register_blueprint(main_views.bp)
-    # app.register_blueprint(coin_views.bp)
+
+    from src.views import coin_views, main_views
+    app.register_blueprint(main_views.bp)
+    app.register_blueprint(coin_views.bp)
+
     return app
