49ccfbbb224ed28c397c0ecd1e3c6ed18e829199
201411108 <aggsae@gmail.com>
2021-09-10 16:27:09 +0900

    feat: get coin info into chart items
    받아온 코인 정보를 차트에 표현하는 기능 구현

--- frontend/src/Components/HomePage/Chart/ChartContainer.tsx
+++ frontend/src/Components/HomePage/Chart/ChartContainer.tsx
@@ -1,9 +1,29 @@
+/* eslint-disable camelcase */
 import React from 'react'
 import './ChartContainer.css'
+import ChartItem from './ChartItem'
 
-function ChartContainer() {
+interface CoinInfo {
+    buy_price?: number;
+    buy_time?: string;
+    fee?: number;
+    name?: string;
+    ticker?: string;
+}
+
+interface Coins {
+    coins: [CoinInfo]
+}
+
+function ChartContainer({ coins }: Coins) {
     return (
-        <div className='chartContainer'>차트 컨테이너</div>
+        <>
+            <div className='chartContainer'>
+                { coins.map((coin) => (
+                    <ChartItem key={coin.ticker} buyPrice={coin.buy_price} buyTime={coin.buy_time} fee={coin.fee} name={coin.name} ticker={coin.ticker} />
+                ))}
+            </div>
+        </>
     )
 }
 
