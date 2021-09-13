f3a971823d4805066a87c513bbeebf40c7cf938b
201411108 <aggsae@gmail.com>
2021-09-14 00:03:52 +0900

    style: adjust properties position
    항목 위치 변경, 스타일 지정

--- frontend/src/Components/HomePage/Chart/ChartItem.tsx
+++ frontend/src/Components/HomePage/Chart/ChartItem.tsx
@@ -1,21 +1,26 @@
 import React from 'react'
+import './ChartItem.css'
 
+// TODO :: 안내와 구분하고 | string 부분 제거
 interface CoinInfo {
-    buyPrice?: number;
-    buyTime?: string;
-    fee?: number;
-    name?: string;
-    ticker?: string;
+    buyPrice?: number | string; // 구매가
+    buyTime?: string; // 구매 시점
+    fee?: number | string;
+    name?: string; // 코인 이름(KR)
+    quantity?: number | string; // 보유 자산
+    ticker?: string; // 코인 약어
 }
 
-function ChartItem({ buyPrice, buyTime, fee, name, ticker}: CoinInfo) {
+function ChartItem({ buyPrice, buyTime, fee, name, quantity, ticker}: CoinInfo) {
     return (
         <>
-            <div>{ buyPrice }</div>
-            <div>{ buyTime }</div>
-            <div>{ fee }</div>
-            <div>{ name }</div>
-            <div>{ ticker }</div>
+            <div className='chartItemContainer'>
+                <div className='chartItem'>{ name }/{ ticker }</div>
+                <div className='chartItem'>{ buyPrice?.toLocaleString('ko-KR') }</div>
+                <div className='chartItem'>{ quantity }</div>
+                <div className='chartItem'>{ buyTime }</div>
+                <div className='chartItem'>{ fee }</div>
+            </div>
         </>
     )
 }
