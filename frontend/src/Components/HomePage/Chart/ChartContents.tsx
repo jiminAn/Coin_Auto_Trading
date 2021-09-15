1f53107822559a428e0a4465e050fc445ebcb0b8
201411108 <aggsae@gmail.com>
2021-09-15 23:49:11 +0900

    style: add chart content style

--- frontend/src/Components/HomePage/Chart/ChartContents.tsx
+++ frontend/src/Components/HomePage/Chart/ChartContents.tsx
@@ -1,13 +1,14 @@
 import React from 'react'
+import './ChartContents.css'
 
 function ChartContents() {
     return (
         <>
             <div className='chartItemContainer'>
-                <div className='chartItem'>자산</div>
-                <div className='chartItem'>구매가(원)</div>
-                <div className='chartItem'>수량</div>
-                <div className='chartItem'>구매시간</div>
+                <div className='chartContent'>자산</div>
+                <div className='chartContent'>구매가(원)</div>
+                <div className='chartContent'>수량</div>
+                <div className='chartContent'>구매시간</div>
             </div>
         </>
     )
