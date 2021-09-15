8efbcb9b54b9ab30accd2bfb4229cff78041cf09
201411108 <aggsae@gmail.com>
2021-09-15 23:45:11 +0900

    feat: divide chart contents from ChartItem

---
+++ frontend/src/Components/HomePage/Chart/ChartContents.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function ChartContents() {
+    return (
+        <>
+            <div className='chartItemContainer'>
+                <div className='chartItem'>자산</div>
+                <div className='chartItem'>구매가(원)</div>
+                <div className='chartItem'>수량</div>
+                <div className='chartItem'>구매시간</div>
+            </div>
+        </>
+    )
+}
+
+export default ChartContents
