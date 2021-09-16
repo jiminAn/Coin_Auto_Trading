c754eb7336b121c0d62af2d3df3bef9d5fb5654d
201411108 <aggsae@gmail.com>
2021-09-16 23:06:41 +0900

    style: adjust chartItem style

--- frontend/src/Components/HomePage/Chart/ChartItem.tsx
+++ frontend/src/Components/HomePage/Chart/ChartItem.tsx
@@ -2,26 +2,33 @@ import React from 'react'
 import moment from 'moment-timezone'
 import './ChartItem.css'
 
-// TODO :: 안내와 구분하고 | string 부분 제거
 interface CoinInfo {
-    buyPrice?: number | string; // 구매가
+    buyPrice?: number; // 구매가
     buyTime?: string; // 구매 시점
-    fee?: number | string;
+    fee?: number;
     name?: string; // 코인 이름(KR)
-    quantity?: number | string; // 보유 자산
+    quantity?: number; // 보유 자산
     ticker?: string; // 코인 약어
 }
 
 function ChartItem({ buyPrice, buyTime, fee, name, quantity, ticker}: CoinInfo) {
-    const time = moment(buyTime)
-    console.log(time.tz('Asia/Seoul').format()) // 2021-09-08T20:24:45+09:00 -> 파싱해서 사용
+    const kstTime = moment(buyTime).tz('Asia/Seoul').format().split('T')
+    const date = kstTime[0] // 2021-09-08
+    const time = kstTime[1].split('+')[0] // 20:24:45
+
     return (
         <>
             <div className='chartItemContainer'>
-                <div className='chartItem'>{ name }/{ ticker }</div>
+                <div className='chartItem'>
+                    <div className='name'>{ name }</div>
+                    <div className='ticker'>/{ ticker }</div>
+                </div>
                 <div className='chartItem'>{ buyPrice?.toLocaleString('ko-KR') }</div>
                 <div className='chartItem'>{ quantity }</div>
-                <div className='chartItem'>{ buyTime }</div>
+                <div className='chartItem'>
+                    <div>{ date }</div>
+                    <div>{ time }</div>
+                </div>
                 {/* <div className='chartItem'>{ fee }</div> */}
             </div>
         </>
