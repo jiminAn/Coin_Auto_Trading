5066133d3f67e53082669139262851a09e9113db
201411108 <aggsae@gmail.com>
2021-09-23 23:19:16 +0900

    feat: add coin components
    변경된 ui 요구사항에 따른 coin components 작성

--- frontend/src/Pages/HomePage.tsx
+++ frontend/src/Pages/HomePage.tsx
@@ -1,17 +1,12 @@
 import React, { useEffect , useState } from 'react'
 import './HomePage.css'
 import ChartContainer from 'Components/HomePage/Chart/ChartContainer'
-import SalesContainer from 'Components/HomePage/Sales/SalesContainer'
+import CoinsContainer from 'Components/HomePage/Coins/CoinsContainer'
 
 async function getCoins() {
     return fetch('http://localhost:5000/coin')
     .then(data => data.json())
 }
-
-/* TODO
-    1. 화면 다시 켜질 때 다시 로그인하도록 하는 것 -> sessionStorage 확인
-    2. sales에서 초기값은 가장 먼저 있는 놈으로 뜨도록 설정
-*/
 
 function HomePage() {
     const [coins, setCoins] = useState<any>([{
@@ -30,7 +25,7 @@ function HomePage() {
     return (
         <div className='homeContainer'>
             {/* coins정보 전달 */}
-            <SalesContainer prevCoins={ coins }/>
+            <CoinsContainer coins={ coins } />
             <ChartContainer coins={ coins }/>
         </div>
     )
