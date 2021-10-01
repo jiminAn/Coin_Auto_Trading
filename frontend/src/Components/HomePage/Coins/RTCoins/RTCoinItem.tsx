b4d03d0b0618ca0cdf3252382fb73584d2d07300
201411108 <aggsae@gmail.com>
2021-10-01 20:22:21 +0900

    feat: add components for real time information
    실시간 정보를 표시할 컴포넌트 작성
    비어 있는 정보 수신에 대한 에러 처리 완료

---
+++ frontend/src/Components/HomePage/Coins/RTCoins/RTCoinItem.tsx
@@ -0,0 +1,39 @@
+/* eslint-disable camelcase */
+import React from 'react'
+import './RTCoinItem.css'
+
+// NOTICE :: 숫자인데 string인 값들은 바뀔 예정
+interface RTCoinInfo {
+    chgAmt?: number;
+    chgRate?: number;
+    cur_price?: number;
+    value?: number;
+}
+
+interface RTCoinProps {
+    rtCoin?: RTCoinInfo
+}
+
+function RTCoinItem({ rtCoin }: RTCoinProps) {
+    // props가 비어있을 경우 undefined
+    if(rtCoin === undefined) {
+        return (
+            <div className='ownedRTCoinContainer'>
+                <div className='ownedRTCoinItem'>해당 코인 정보를 불러올 수 없습니다.</div>
+            </div>
+        )
+    }
+    const { chgAmt, chgRate, cur_price, value } = rtCoin
+    // console.log(rtCoin)
+
+    return (
+        <div className='ownedRTCoinContainer'>
+            <div className='ownedRTCoinItem'>{ cur_price?.toLocaleString('ko-KR') }</div>
+            <div className='ownedRTCoinItem'>{ chgAmt?.toLocaleString('ko-KR') }</div>
+            <div className='ownedRTCoinItem'>{ chgRate?.toLocaleString('ko-KR') }</div>
+            <div className='ownedRTCoinItem'>{ value?.toLocaleString('ko-KR') }</div>
+        </div>
+    )
+}
+
+export default RTCoinItem
