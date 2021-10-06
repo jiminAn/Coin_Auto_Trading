82c8b7d7ae18ea7e54e0b2b78e18902f1c0d7e28
jiminAn <als398@naver.com>
2021-10-06 12:24:05 +0900

    style : change single qoute

--- backend/src/pybithumb/ApiConnect.py
+++ backend/src/pybithumb/ApiConnect.py
@@ -15,40 +15,40 @@ class Connect:
     """
 
     def __init__(self):
-        """
+        '''
         intialize secrete key and connect key to empty string
-        """
+        '''
         self._sec_key = ""
         self._con_key = ""
 
     def log_in(self, con_key, sec_key):
-        """
+        '''
         Log in with client's connect key and secrete key
         :param con_key: connect key(Str)
         :parma sec_key: secrete key(Str)
-        """
+        '''
         self._con_key = con_key
         self._sec_key = sec_key
 
     def get_bithumb(self):
-        """
+        '''
         initialize client Bithumb instance
         :return: Bithumb instance
-        """
+        '''
         return pybithumb.Bithumb(self._con_key, self._sec_key)
 
     def get_con_key(self):
-        """
+        '''
         get connect key for API
         :return: con_key (String)
-        """
+        '''
         return self._con_key
 
     def is_api_key_valid(self):
-        """
+        '''
         validate the api key
         :return: Invalid APIkey(False)/ valud APIkey(True)
-        """
+        '''
 
         recv_data = pybithumb.Bithumb(self._con_key, self._sec_key).get_balance("BTC")
 
