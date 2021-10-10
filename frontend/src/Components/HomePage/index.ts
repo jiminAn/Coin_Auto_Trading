2bb81c558da01fa2a438babc41120d4dafdc6e7f
201411108 <aggsae@gmail.com>
2021-10-10 23:42:43 +0900

    refactor: add index.ts for components used in CoinsContainer

--- frontend/src/Components/HomePage/index.ts
+++ frontend/src/Components/HomePage/index.ts
@@ -4,6 +4,6 @@ import CoinInfoItem from 'Components/HomePage/Coins/CoinsInfo/CoinInfoItem'
 import OwnedCoinContents from 'Components/HomePage/Coins/OwnedCoins/OwnedCoinContents'
 import OwnedCoinItem from 'Components/HomePage/Coins/OwnedCoins/OwnedCoinItem'
 import TradingLogs from 'Components/HomePage/Logs/TradingLogs'
-import RTCoinItem from 'Components/HomePage/Coins/RTCoins/RTCoinItem'
+import RTCoinItem from 'Components/HomePage/RTCoins/RTCoinItem'
 
 export { ChartContainer, CoinInfoContents, CoinInfoItem, OwnedCoinContents, OwnedCoinItem, TradingLogs, RTCoinItem }
